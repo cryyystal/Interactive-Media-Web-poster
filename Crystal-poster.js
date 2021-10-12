6e508f4ead9753e8131f19a68d59644f6db40774
Crystal Wong <crystalwong-cw@hotmail.com>
2021-10-11 22:56:12 -0400

    a

--- Crystal-poster.js
+++ Crystal-poster.js
@@ -1,13 +1,3 @@
-// var song1  = new Audio();
-// var src1  = document.createElement("source");
-// src1.type = "audio/mpeg";
-// src1.src  = "carnival.mp3";
-// song1.appendChild(src1);
-// song1.play();
-
-// var song2  = new Audio();
-// var src2  = document.createElement("source");
-// src2.type = "audio/mpeg";
-// src2.src  = "carnival 2.mp3";
-// song2.appendChild(src2);
-// song2.play();
+function myFunction() {
+  location.replace("https://www.cirquedusoleil.com")
+}
